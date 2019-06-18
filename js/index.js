014e3a8a8ce3305a226e49e3779305fb45cbb395
Marcus Jones <indelibleink84@gmail.com>
2019-06-18 17:48:35 -0500

    updated the na with mouseover color, dblclick green, and onblur pink for the title

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // Your code goes here
+
+
+let navLinks = document.querySelector('nav');
+
+    navLinks.addEventListener('mouseover', event =>{
+        event.target.style.color = 'blue'});
+
+    navLinks.addEventListener('mouseout', event =>{
+        event.target.style.color = 'blue'});
+
+    navLinks.addEventListener('dblclick', event =>{
+        event.target.style.color = 'green'});
+    
+const header = document.querySelector('h1');
+
+    header.addEventListener('onblur', (event) => {
+        event.target.style.background = 'pink';    
+    });
+
+    header.addEventListener('blur', (event) => {
+        event.target.style.background = '';    
+    });
+
